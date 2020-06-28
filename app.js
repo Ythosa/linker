ac3abdf357a231665eb1d16ddb703488257e79c3
ythosa <vasus714@yandex.ru>
2020-06-28 16:19:57 +0300

    mongoose connection

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 
+app.use('/api/auth', require('./routes/auth.routes'))
+
 const PORT = config.get('port') || 5000
+const MONGOURI = config.get('mongoUri')
 
-app.listen(PORT, () => {
-    console.log(`App has been started on port ${PORT}...`)
-})
+async function start() {
+    try {
+        await mongoose.connect(MONGOURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
 
+    app.listen(PORT, () => {
+        console.log(`App has been started on port ${PORT}...`)
+    })
+}
 
+start()
