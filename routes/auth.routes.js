78741478a5390a730935b94daefa6cc5425d5f66
ythosa <vasus714@yandex.ru>
2020-06-28 16:48:17 +0300

    register api method

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,40 @@
-const {Router} = require('express')
+const { Router } = require('express')
+const bcrypt = require('bcrypt')
+const { check, validationResult } = require('express-validator')
 const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
+router.post(
+    '/register',
+    [
+        check('email', 'Invalid email address.').isEmail(),
+        check('password', 'Password minimum length is 6.')
+            .isLength({ min: 6 })
+    ],
+    async (req, res) => {
     try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Invalid data during registration'
+            })
+        }
+
         const { email, password } = req.body
+
+        const candidate = await User.findOne({ email })
+        if (candidate) {
+            return res.status(400).json({ message: 'This user is exist' })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({ email, password: hashedPassword })
+
+        await user.save()
+
+        res.status(201).json({ message: 'The user is created' })
 
     } catch (e) {
         res.status(500).json({ message: "Something went wrong. Try again :("})
