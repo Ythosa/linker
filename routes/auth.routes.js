975690ed0140dfc6f99e632ada222ab0b78d22f0
ythosa <vasus714@yandex.ru>
2020-06-28 16:34:44 +0300

    user model

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,15 @@
 const {Router} = require('express')
+const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
 router.post('/register', async (req, res) => {
+    try {
+        const { email, password } = req.body
 
+    } catch (e) {
+        res.status(500).json({ message: "Something went wrong. Try again :("})
+    }
 })
 
 // /api/auth/login
