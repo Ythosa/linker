51d738d7abfca5cb340e482710803e9e954a6f47
ythosa <vasus714@yandex.ru>
2020-06-29 21:18:36 +0300

    fix inputs

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -23,10 +23,10 @@ export const Navbar = () => {
         <React.Fragment>
         <nav>
             <div className="nav-wrapper deep-purple lighten-3">
-                <a href="/" className="brand-logo nav_logo">
+                <span className="brand-logo nav_logo">
                     <i className="material-icons">photo_size_select_small</i>
                     Linker
-                </a>
+                </span>
                 <a href="/" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                 <ul className="right hide-on-med-and-down">
                     <li><NavLink to="/create">Create</NavLink></li>
